cd57d928904e13469d74d3fd0153bcf1b820e190
Steven Gann <stevengannjr@gmail.com>
2024-07-04 14:48:34 -0700

    Documented main HyperVectorDB class

--- HyperVectorDB/HyperVectorDB.cs
+++ HyperVectorDB/HyperVectorDB.cs
@@ -11,16 +11,38 @@ using HyperVectorDB.Embedder;
 
 namespace HyperVectorDB
 {
-
+    /// <summary>
+    /// The top level object of the vector database system, and the center point of the API.
+    /// </summary>
     public class HyperVectorDB
     {
 
+        /// <summary>
+        /// Path to the directory where the database will be stored on disk.
+        /// </summary>
         public readonly string DatabasePath;
 
+        /// <summary>
+        /// Optional delegate for processing text prior to vectorization. If this delegate returns `null` the vectorization and storage will be skipped.
+        /// </summary>
+        /// <param name="line">Text to process</param>
+        /// <param name="path">Path to file text originated from, if applicable</param>
+        /// <param name="lineNumber">Line number of file text originated from, if applicable</param>
+        /// <returns></returns>
         public delegate string? DocumentPreprocessor(string line, string? path = null, int? lineNumber = null);
 
+        /// <summary>
+        /// Optional delegate for processing text prior to storage in the database. If this delegate returns `null` the vectorization and storage will be skipped.
+        /// </summary>
+        /// <param name="line">Text to process</param>
+        /// <param name="path">Path to file text originated from, if applicable</param>
+        /// <param name="lineNumber">Line number of file text originated from, if applicable</param>
+        /// <returns></returns>
         public delegate string? DocumentPostprocessor(string line, string? path = null, int? lineNumber = null);
 
+        /// <summary>
+        /// Collection of `HyperVectorDBIndex` organized by names
+        /// </summary>
         public readonly Dictionary<string, HyperVectorDBIndex> Indexes;
 
         private readonly IEmbedder _embedder;
@@ -28,6 +50,12 @@ namespace HyperVectorDB
         private ulong AutoIndexCount = 0;
 
 
+        /// <summary>
+        /// If `autoIndexCount` is ommited or less than 1, the Auto Index functionality will be disabled. See the [Auto Index documentation](xref:AutoIndexDoc) for more information.
+        /// </summary>
+        /// <param name="embedder">The `IEmbedder` class to provide text embedding functionality</param>
+        /// <param name="path">Path to the directory where the database will be stored on disk</param>
+        /// <param name="autoIndexCount">Optional. Number of automatic indexes to generate and use</param>
         public HyperVectorDB(IEmbedder embedder, string path, int autoIndexCount = 0)
         {
             _embedder = embedder;
@@ -48,6 +76,11 @@ namespace HyperVectorDB
             }
         }
 
+        /// <summary>
+        /// Creates a named index. Note that every index must have a name that is unique within the current `HyperVectorDB`.
+        /// </summary>
+        /// <param name="name">Unique name for the index</param>
+        /// <returns>`True` if creation was successful, `False` if an error was encountered1</returns>
         public bool CreateIndex(string name)
         {
             if (Indexes.ContainsKey(name))
@@ -59,6 +92,11 @@ namespace HyperVectorDB
             return true;
         }
 
+        /// <summary>
+        /// Deletes an index with the given name, if it exists.
+        /// </summary>
+        /// <param name="name">Name of the index to be deleted</param>
+        /// <returns>`True` if an index with a matching name was found and removed. `False` if the `HyperVectorDB` does not contain an index with the specified name</returns>
         public bool DeleteIndex(string name)
         {
             if (!Indexes.ContainsKey(name))
@@ -69,12 +107,29 @@ namespace HyperVectorDB
             return true;
         }
 
+        /// <summary>
+        /// Indexes a single string.
+        /// Note: This overload is a pass-through for backwards compatibility with previous versions.
+        /// </summary>
+        /// <param name="indexName">Name of the index to store in</param>
+        /// <param name="document">Text to be vectorized and indexed</param>
+        /// <param name="preprocessor">Optional preprocessor delegate to process text prior to vectorization</param>
+        /// <param name="postprocessor">Optional postrpocessor delegate to process text prior to storage in database</param>
+        /// <returns>`True` if the text was vectorized and stored without issue. `False` if an error was encountered or if the Preprocessor or Postprocessor delegates returned `null`</returns>
         public bool IndexDocument(string indexName, string document, DocumentPreprocessor? preprocessor = null, DocumentPostprocessor? postprocessor = null)
         {
             //Passthrough overload to avoid breaking changes to API
             return IndexDocument(document, preprocessor, postprocessor, indexName);
         }
 
+        /// <summary>
+        /// Indexes a single string.
+        /// </summary>
+        /// <param name="document">Text to be vectorized and indexed</param>
+        /// <param name="preprocessor">Optional preprocessor delegate to process text prior to vectorization</param>
+        /// <param name="postprocessor">Optional postrpocessor delegate to process text prior to storage in database</param>
+        /// <param name="indexName">Optional. Name of the index to store in. If ommited, an index will be chosen automatically</param>
+        /// <returns>`True` if the text was vectorized and stored without issue. `False` if an error was encountered or if the Preprocessor or Postprocessor delegates returned `null`</returns>
         public bool IndexDocument(string document, DocumentPreprocessor? preprocessor = null, DocumentPostprocessor? postprocessor = null, string? indexName = null)
         {
             string IndexName;
@@ -123,11 +178,28 @@ namespace HyperVectorDB
             }
         }
 
+        /// <summary>
+        /// Indexes every line in a file, sequentially
+        /// Note: This overload is a pass-through for backwards compatibility with previous versions.
+        /// </summary>
+        /// <param name="indexName">Name of the index to store in</param>
+        /// <param name="documentPath">Path to the file to be indexed</param>
+        /// <param name="preprocessor">Optional preprocessor delegate to process text prior to vectorization</param>
+        /// <param name="postprocessor">Optional postrpocessor delegate to process text prior to storage in database</param>
+        /// <returns></returns>
         public bool IndexDocumentFile(string indexName, string documentPath, DocumentPreprocessor? preprocessor = null, DocumentPostprocessor? postprocessor = null)
         {
             return IndexDocumentFile(documentPath, preprocessor, postprocessor, indexName);
         }
 
+        /// <summary>
+        /// Indexes every line in a file, sequentially
+        /// </summary>
+        /// <param name="documentPath">Path to the file to be indexed</param>
+        /// <param name="preprocessor">Optional preprocessor delegate to process text prior to vectorization</param>
+        /// <param name="postprocessor">Optional postrpocessor delegate to process text prior to storage in database</param>
+        /// <param name="indexName">Optional. Name of the index to store in. If ommited, an index will be chosen automatically</param>
+        /// <returns></returns>
         public bool IndexDocumentFile(string documentPath, DocumentPreprocessor? preprocessor = null, DocumentPostprocessor? postprocessor = null, string? indexName = null)
         {
             if (!System.IO.File.Exists(documentPath)) return false;
@@ -182,6 +254,10 @@ namespace HyperVectorDB
             return true;
         }
 
+        /// <summary>
+        /// Saves the entire database to disk. Every index is stored in its own files.
+        /// The files will not be overwritten if the index hasn't changed since a `Load()` or `Save()`.
+        /// </summary>
         public void Save()
         {
             if (!Directory.Exists(DatabasePath)) Directory.CreateDirectory(DatabasePath);
@@ -195,13 +271,16 @@ namespace HyperVectorDB
             sw.Close();
 
             Console.WriteLine("Index Usage:");
-            foreach(string key in Indexes.Keys)
+            foreach (string key in Indexes.Keys)
             {
                 Console.Write($"{Indexes[key].Count,4}");
             }
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Loads the entire database from disk. Existing indexes with duplicate names are overwritten from disk.
+        /// </summary>
         public void Load()
         {
             var indexfile = System.IO.Path.Combine(DatabasePath, "indexs.txt");
@@ -224,6 +303,12 @@ namespace HyperVectorDB
             sr.Close();
         }
 
+        /// <summary>
+        /// Queries all indexes of the current `HyperVectorDB` in parallel to find similar entries.
+        /// </summary>
+        /// <param name="query">Text to be used as query</param>
+        /// <param name="topK">Optional, indicates how many results to return</param>
+        /// <returns></returns>
         public HVDBQueryResult QueryCosineSimilarity(string query, int topK = 5)
         {
             var vector = _embedder.GetVector(query);
