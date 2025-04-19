1f235efb5ca35b17ae99f4f48e1ba7d6e3250c5e
Steven Gann <stevengannjr@gmail.com>
2025-04-18 23:05:25 -0700

    Cleanup and add basic code documentation

--- HyperVectorDB/Embedder/EmbedderOpenAI-ADA-002.cs
+++ HyperVectorDB/Embedder/EmbedderOpenAI-ADA-002.cs
@@ -6,18 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HyperVectorDB.Embedder {
+    /// <summary>
+    /// Implementation of IEmbedder that uses OpenAI's text-embedding-ada-002 model to convert text into vector representations.
+    /// </summary>
     public class EmbedderOpenAI_ADA_002 : IEmbedder {
         private readonly OpenAI.OpenAIClient Client;
+        /// <summary>
+        /// Tracks the total number of tokens used across all embedding requests.
+        /// </summary>
         public static int TotalTokens = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the EmbedderOpenAI_ADA_002 class.
+        /// </summary>
+        /// <param name="openaiapikey">The API key for accessing OpenAI's services.</param>
         public EmbedderOpenAI_ADA_002(string openaiapikey) {
             this.Client = new OpenAI.OpenAIClient(new OpenAI.OpenAIAuthentication(openaiapikey));
         }
+
+        /// <inheritdoc/>
         public Double[] GetVector(String Document) {
             var result = this.Client.EmbeddingsEndpoint.CreateEmbeddingAsync(Document, OpenAI.Models.Model.Embedding_Ada_002).GetAwaiter().GetResult();
             TotalTokens += result.Usage.TotalTokens ?? 0; //TODO: Check if this is correct and why openai made this nullable
-			var vect = result.Data[0].Embedding.ToArray<double>();
+            var vect = result.Data[0].Embedding.ToArray<double>();
             return vect;
         }
+
+        /// <inheritdoc/>
         public Double[][] GetVectors(String[] Documents) {
             var result = this.Client.EmbeddingsEndpoint.CreateEmbeddingAsync(Documents, OpenAI.Models.Model.Embedding_Ada_002).GetAwaiter().GetResult();
             TotalTokens += result.Usage.TotalTokens ?? 0; //TODO: Check if this is correct and why openai made this nullable
